e9f1b952294c20ad86f0a113101487c158aea7fa
TakahoItaya <84277209+TakahoItaya@users.noreply.github.com>
2021-05-23 22:07:07 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(function(){
+    $('#nav-btn').on('click' ,function(){
+        $(this).toggleClass('-active');
+        $('#nav').toggleClass('-active');
+    });
+});
+
+$('a[href^="#"]').click(function(){
+    var speed = 800;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+});
+
+$('#nav a[href]').on('click', function(event) {
+    $('#nav-btn').trigger('click');
+});
